20d30bc6b1d278cdbdd921aba5d440650f23850d
digis <digis@ALIEN73>
2024-03-13 18:58:33 -0600

    Add project files.

---
+++ Expense Tracker/Migrations/20240312204746_agregarcolor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Expense_Tracker.Migrations
+{
+    public partial class agregarcolor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Color",
+                table: "Categories",
+                type: "nvarchar(5)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Color",
+                table: "Categories");
+        }
+    }
+}
